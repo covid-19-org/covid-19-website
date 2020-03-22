28faf9abf2cebb15fe9b0024c4e0c17ea23234d1
Pete Taylour <pete.taylour@gmail.com>
2020-03-22 23:12:18 +0000

    add nav menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,14 @@
 import PropTypes from "prop-types"
+import { Button, Flex, Stack, IconButton } from "@chakra-ui/core"
 import React from "react"
 import code4covid from "../images/code4covid.svg"
 import { Link } from "gatsby"
+
+const PAGES = [
+  { children: "home", to: "/" },
+  { children: "page 1", to: "/" },
+  { children: "page 2", to: "/" },
+]
 
 const Header = () => (
   <header
@@ -9,12 +16,11 @@ const Header = () => (
       background: `white`,
     }}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 1300,
-        padding: `1.45rem 1.0875rem`,
-      }}
+    <Flex
+      margin="0 auto"
+      maxWidth={1300}
+      padding="1.45rem 1.0875rem"
+      justifyContent="space-between"
     >
       <Link to="/">
         <img
@@ -23,7 +29,30 @@ const Header = () => (
           alt="code4covidLogo"
         />
       </Link>
-    </div>
+      <IconButton
+        display={["block", "block", "none"]} // Match this with the inverse of the buttons stack to use Chakra breakpoints
+        variant="ghost"
+        size="lg"
+        aria-label="Navigation Button"
+        icon="menu"
+      />
+      <Stack
+        display={["none", "none", "block"]}
+        as="nav"
+        direction="row"
+        spacing={2}
+      >
+        {PAGES.map(page => (
+          <Button
+            variant="ghost"
+            size="lg"
+            key={page.children + page.to}
+            as={Link}
+            {...page}
+          />
+        ))}
+      </Stack>
+    </Flex>
   </header>
 )
 
