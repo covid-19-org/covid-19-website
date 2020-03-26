c5ebe6fe5b5b5148e9775619e72d9f2f012039a9
Keith Broughton <keithbro256@gmail.com>
2020-03-26 11:26:18 +1100

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,12 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-152512806-2",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
