80e264c181fabd510ccf23b5afe45faf9edce297
Emile <emilepw@hotmail.co.uk>
2020-03-31 18:24:47 +0100

    Add partner with us Airtable form in a new page

---
+++ src/pages/partner-with-us.js
@@ -0,0 +1,33 @@
+import React, { useRef } from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { Spinner, Box } from "@chakra-ui/core"
+
+const PartnerWithUs = () => {
+  const spinnerRef = useRef()
+
+  const hideSpinner = () => (spinnerRef.current.style.display = "none")
+
+  return (
+    <Layout>
+      <SEO title="Partner With Us" />
+      <Box textAlign="center">
+        <Spinner ref={spinnerRef} marginTop={200} />
+
+        <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js" />
+        <iframe
+          title="Partner with Us Form"
+          class="airtable-embed airtable-dynamic-height"
+          src="https://airtable.com/embed/shrtSXiyO1dPOOfiW?backgroundColor=yellow"
+          frameBorder="0"
+          width="100%"
+          height="1940"
+          background="transparent"
+          onLoad={hideSpinner}
+        />
+      </Box>
+    </Layout>
+  )
+}
+
+export default PartnerWithUs
