17fbac2a40e2cb8fcc6b8778c5dc0208d01fdecb
Keith Broughton <keithbro256@gmail.com>
2020-04-02 18:55:22 +1100

    Wrestling with Amplify

--- dev-bin/import.sh
+++ dev-bin/import.sh
@@ -6,14 +6,14 @@ set -e
 IFS='|'
 
 AWSCLOUDFORMATIONCONFIG="{\
-  \"AuthRoleName\": \"amplify-covid-19-website-dev-163103-authRole\",\
-  \"UnauthRoleArn\": \"arn:aws:iam::954225943395:role/amplify-covid-19-website-dev-163103-unauthRole\",\
-  \"AuthRoleArn\": \"arn:aws:iam::954225943395:role/amplify-covid-19-website-dev-163103-authRole\",\
+  \"AuthRoleName\": \"covid-19-website-develop-20200402073645-authRole\",\
+  \"UnauthRoleArn\": \"arn:aws:iam::954225943395:role/covid-19-website-develop-20200402073645-unauthRole\",\
+  \"AuthRoleArn\": \"arn:aws:iam::954225943395:role/covid-19-website-develop-20200402073645-authRole\",\
   \"Region\": \"eu-west-2\",\
-  \"DeploymentBucketName\": \"amplify-covid-19-website-dev-163103-deployment\",\
-  \"UnauthRoleName\": \"amplify-covid-19-website-dev-163103-unauthRole\",\
-  \"StackName\": \"amplify-covid-19-website-dev-163103\",\
-  \"StackId\": \"arn:aws:cloudformation:eu-west-2:954225943395:stack/amplify-covid-19-website-dev-163103/289a6600-74a3-11ea-9701-0617023ccf6e\",\
+  \"DeploymentBucketName\": \"covid-19-website-develop-20200402073645-deployment\",\
+  \"UnauthRoleName\": \"covid-19-website-develop-20200402073645-unauthRole\",\
+  \"StackName\": \"covid-19-website-develop-20200402073645\",\
+  \"StackId\": \"arn:aws:cloudformation:eu-west-2:954225943395:stack/covid-19-website-develop-20200402073645/b46e3c40-74b4-11ea-8f3c-069b836e2b40\",\
   \"AmplifyAppId\": \"du43hqzd9ye4n\"\
 }"
 
