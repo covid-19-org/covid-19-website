a6dea35cccfac82c345a4bef7eff580d6e3da12a
Rob Morrissey <robmorrissey@gmail.com>
2020-04-14 14:40:14 +0100

    Minor changes to tech support flow.

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ const IndexPage = () => (
     >
       <Heading {...headerStyling}>Need tech resources <br/> for a project?</Heading>
       <Link href="https://covidtechsupport.com/" isExternal>
-        <Button {...buttonStyling}>GET TECH SUPPORT</Button>
+        <Button {...buttonStyling}>GET TECH RESOURCES</Button>
       </Link>
       <Heading {...headerStyling}>
         Tech Volunteer? Join the <i>#code4covid</i> Community
@@ -73,6 +73,9 @@ const IndexPage = () => (
         </ListItem>
         <ListItem>
           <Link textDecoration="underline" fontWeight={500} _hover={{ fontWeight: 700 }} href="https://helpwithcovid.com" isExternal>helpwithcovid.com</Link> - International volunteer sign ups and ideas board
+        </ListItem>
+        <ListItem>
+          <Link textDecoration="underline" fontWeight={500} _hover={{ fontWeight: 700 }} href="https://covidtechsupport.com/" isExternal>covidtechsupport.com</Link> - Platform to request tech support
         </ListItem>
       </List>
     </Section>
